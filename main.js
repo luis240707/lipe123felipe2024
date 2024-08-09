725255230e8d8edeb30c59691c09b2c2047210ce
luis240707 <128099846+luis240707@users.noreply.github.com>
2024-08-08 22:25:32 -0300

    Update main.js
    aaa

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ for(let i=0;i <botoes.length;i++){
         texto[j].classList.remove("ativo");
     }
          botoes[i].classList.add("ativo");
-         texto[i].classList.add ("ativo");
+         texto [i].classList.add ("ativo");
 
     }
 }
@@ -18,4 +18,4 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 -  tempoAtual;
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
